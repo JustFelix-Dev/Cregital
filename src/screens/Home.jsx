5b1ccc6d42107fb0983e62ad833172af0e0be3a3
Felix Owolabi <owolabifelix78@gmail.com>
2024-03-10 11:59:39 +0100

    feat: completed who we are

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar"
 import dropLine from "../assets/Link.svg";
 import Hero from "../components/Hero";
 import Works from "../components/Works";
+import WhoWeAre from "../components/WhoWeAre";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Navbar/>
         <Hero/>
         <Works/>
+        <WhoWeAre/>
         <div className=" bg-white fixed bottom-[1rem] right-[1rem]">
             <img  className="dropline" src={dropLine} alt="drop-line" />
         </div>
