f5a6dd7736f73913041f2561ffb9c78a56702e5a
Felix Owolabi <owolabifelix78@gmail.com>
2024-03-10 14:49:00 +0100

    feat: completed cregital

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
 import logo from "../assets/cregital-logo.svg";
 import menuOpen from "../assets/menu.svg";
+import cancelMenu from "../assets/cancelmenu.svg";
 
 const Navbar = () => {
+    const [ isMenuOpen,setIsMenuOpen] = useState(false);
+
+    const toggleMenu = ()=>{
+        setIsMenuOpen(!isMenuOpen)
+    }
   return (
+    <>
     <nav className="h-[100px] max-w-[90%] w-full mx-auto bg-white flex items-center justify-between">
       <div className="logo"><img src={logo} alt="cregital-logo" /></div>
-      <div className="flex items-center gap-4" >
+      <div onClick={toggleMenu} className="flex items-center gap-4" >
         <img  className='menu' src={menuOpen} alt="menu-open" />
         <span className="font-ibm thick-text text-[15px] text-dark font-bold" >MENU</span>
       </div>
     </nav>
+    {
+        isMenuOpen && (
+          <div className="fixed  bg-black z-10 inset-0 items-center justify-center text-white">
+             <nav className="h-[100px] max-w-[90%] w-full mx-auto flex items-center justify-between">
+      <div className="logo"><img src={logo} alt="cregital-logo" /></div>
+      <div onClick={toggleMenu} className="flex items-center gap-4" >
+        <img  className='menu' src={cancelMenu} alt="menu-open" />
+        <span className="font-ibm thick-text text-[15px] text-dark font-bold">CLOSE</span>
+      </div>
+            </nav>
+            <div className="flex mx-auto items-center justify-center hero font-ibm text-[70px] font-semibold max-w-[23ch] text-white text-center">
+                <div className="-mt-8 menu-tight"> Learn more <a className="border-b border-[#aaa] footer-links">about us</a>, check out <a className=" border-b border-[#aaa] footer-links">our work</a>, the latest on <a className="border-b border-[#aaa] footer-links">instagram</a> and <a className="border-b border-[#aaa] footer-links">contact us</a></div>
+               
+            </div>
+          </div>
+        )
+    }
+    </>
   )
 }
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,19 +4,57 @@ import Hero from "../components/Hero";
 import Works from "../components/Works";
 import WhoWeAre from "../components/WhoWeAre";
 import Footer from "../components/Footer";
+import { useState } from "react";
+import cancelMenu from "../assets/cancelmenu.svg";
+import logo from "../assets/cregital-logo.svg";
 
 const Home = () => {
+    const [isDropLine,setIsDropLine] = useState(false);
+
+ const openDropLine = ()=>{
+    setIsDropLine(true)
+ }
+
+ const closeDropLine = ()=>{
+    setIsDropLine(false)
+ }
   return (
+    <>
     <div>
         <Navbar/>
         <Hero/>
         <Works/>
         <WhoWeAre/>
         <Footer/>
-        <div className="dropline fixed bottom-[1.5rem] right-[1.5rem]">
+        <div onClick={openDropLine} className="dropline fixed bottom-[1.5rem] right-[1.5rem]">
             <img  className="dropline" src={dropLine} alt="drop-line" />
         </div>
     </div>
+    { isDropLine && (
+      <div className="fixed bg-black inset-0 flex">
+          <nav className="h-[100px] max-w-[90%] w-full mx-auto flex items-center">
+      <div className="logo"><img src={logo} alt="cregital-logo" /></div>
+            </nav>
+            <div className=" absolute min-h-[calc(100vh-100px)]flex w-full flex items-center inset-0 ">
+                <div className="flex flex-col gap-8 w-full max-w-[684px] mx-auto">
+                <p className="font-ibm text-[70px] font-semibold text-white text-center">Drop us a line</p>
+                <span className="text-white font-light text-[25px] font-ibm text-center">Complete the form or email <span className="footer-links border-b border-[#aaa]">hello@gmail.com</span>.</span>
+                <form action="#"  className="font-ibm flex flex-col gap-4">
+                    <input type="text" placeholder="Name" name="name" id="name" />
+                    <input type="text" placeholder="Phone Number" name="name" id="name" />
+                    <input type="text" placeholder="Email" name="name" id="name" />
+                     <textarea name="message" id="" cols="30" rows="10" placeholder="Briefly tell us about your project"></textarea>
+                     <button className="font-ibm text-[15px] font-semibold bg-white min-h-[59px] w-full flex items-center justify-center hover:bg-[#B5B5B5] transition-all">SEND MESSAGE</button>
+                </form>
+                </div>
+            </div>
+         <div onClick={closeDropLine} className="dropline bg-white p-4 rounded-full fixed bottom-[3rem] right-[3rem]">
+            <img  className="dropline" src={cancelMenu} height={15} width={15} alt="drop-line" />
+        </div>
+      </div>
+    )
+    }
+    </>
   )
 }
 
